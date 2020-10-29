95be44f5e6c01ac6e32c86ecd12846a8b41cda60
limaluca <lucaslimamesmo@gmail.com>
2020-10-29 11:22:44 -0300

    Adding the routes and finishing the server

--- src/server.js
+++ src/server.js
@@ -1,8 +1,33 @@
-function printaAi() {
-    console.log("printou :)")
-}
+//Import dependencies
+const express = require('express');
+const path = require('path');
+const pages = require('./pages.js')
 
-printa = printaAi()
+const server = express();
+
+//Using static files
+server.use(express.static('public'))
+
+//Configuring template engine
+server.set('views', path.join(__dirname, 'views'))
+server.set('view engine', 'hbs')
+
+//New route
+server.get('/', pages.index)
+
+server.get('/orphanage', pages.orphanage)
+
+server.get('/orphanages', pages.orphanages)
+
+server.get('/create-orphanage', pages.createOrphanage)
 
 
-console.log(printa)
+
+
+
+
+
+
+
+// Turn on the server
+server.listen(5500)
