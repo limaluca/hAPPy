ffd09863a9803e426dd11c5869c32bdf287f5aa3
limaluca <lucaslimamesmo@gmail.com>
2020-11-05 17:57:08 -0300

    creating, searching and deleting data from the table

--- src/database/teste.js
+++ src/database/teste.js
@@ -1,37 +1,27 @@
-const Database = require('./db');
+const Database = require("./db");
+const saveOrphanage = require("./saveOrphanage");
 
-Database.then(async db => {
-    // insert data Into the table
-    await db.run(`
-      INSERT INTO orphanages(
-            lat,
-            lng,
-            name,
-            about,
-            whatsapp,
-            images,
-            instructions,
-            opening_hours,
-            open_on_weekends
-      ) VALUES (
-            "-2.522602",
-            "44.654434",
-            "lar dos meninos",
-            "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
-            "989828282",
-            "https://images.unsplash.com/photo-1573261524302-6f1dc1458378?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
-            "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
-            "Horário de visitas Das 18h até 22h",
-            "0"  
+Database.then(async(db) => {
+    await saveOrphanage(db, {
+        lat: "-2.590499",
+        lng: "-44.104078",
+        name: "Lar das meninaaaasssss",
+        about: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social",
+        whatsapp: "55999992222",
+        images: [
+            "https://images.unsplash.com/photo-1563465814437-b1a057a461ed?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+            "https://images.unsplash.com/photo-1594575111057-47b35c5f98f7?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+        ].toString(),
+        instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar",
+        opening_hours: "Horário de visitas Das 18h até 8h",
+        open_on_weekends: "0",
+    });
 
-        
-      
-      );
-`)
+    const selectedOrphanages = await db.all("SELECT * FROM orphanages");
+    console.log(selectedOrphanages);
 
-    //consult data from the table
-    const selectedOrphanages = await db.all("SELECT * FROM orphanages")
-    console.log(selectedOrphanages)
+    const orphanages = await db.all('SELECT * FROM orphanages WHERE id = "1"');
+    console.log(orphanages);
 
-
-})
+    // console.log(await db.run("DELETE FROM orphanages WHERE id = '2'"))
+});
--- src/pages.js
+++ src/pages.js
@@ -1,14 +1,37 @@
-const orphanages = require('./database/fakeData.js')
+const Database = require("./database/db");
+const saveOrphanage = require("./database/saveOrphanage");
 module.exports = {
+
     index(request, response) {
+
         return response.render('index')
     },
-    orphanage(request, response) {
-        return response.render('orphanage')
+    async orphanage(request, response) {
+        const id = request.query.id;
+
+        try {
+            const db = await Database;
+            const orphanage = await db.all(`SELECT * FROM orphanages WHERE id = "${id}"`);
+
+
+            return response.render('orphanage', { orphanage: orphanage[0] })
+
+        } catch (error) {
+            console.log(error)
+            return res.send("Erro no banco de dados")
+
+        }
     },
 
-    orphanages(request, response) {
-        return response.render('orphanages', { orphanages })
+    async orphanages(req, res) {
+        try {
+            const db = await Database;
+            const orphanages = await db.all("SELECT * FROM orphanages");
+            return res.render("orphanages", { orphanages });
+        } catch (error) {
+            console.log(error);
+            return res.send("Erro no banco de dados");
+        }
     },
 
 
